1feac4c9bab0d3f8d304e8b4551dde2a56d3571a
Nguyen Van Toan <77067868+ToanBarcelona1998@users.noreply.github.com>
2024-04-01 09:29:01 +0700

    #added android fol

---
+++ android/app/src/main/kotlin/com/example/trust_wallet_core_example/MainActivity.kt
@@ -0,0 +1,9 @@
+package com.example.trust_wallet_core_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+    init {
+        System.loadLibrary("TrustWalletCore")
+    }
+}
